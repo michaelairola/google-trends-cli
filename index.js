38c5062e6d8b76261aa0103f2a1614d95ec84c65
michaelairola <mikeairola@mac.com>
2020-12-23 09:06:03 -0800

    feat: check environment for timezone and language, only pass in comparison items

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const { 
-  TrendsClient,
-  // getTLWidget, 
-  // getTLTrend,
-} = require("./lib/index.js");
+const { TrendsClient } = require("./lib/index.js");
+// saves about 350ms to pass in cookie
+const cookie = "NID=205=aksdsOWLfmVm8Y529fZmVLh_x_lvcb59R6nO7_fVjb-8NnwXcbTLjsQkNUjcq9womNyTsYXDD_YZIdgh4Ki8S8iouWTnG2ZDbblHR1AswVnRCH5zHQLKWqADC-Uhs0oyZq1goc4WERPIODtfWFaGN15R_oFnZp2jtgVK1GdMpnQ; expires=Thu, 24-Jun-2021 16:34:52 GMT; path=/; domain=.google.com; HttpOnly";
+
 (async () => {try {
-  const trendsCli = new TrendsClient();
-  let e = await trendsCli.authQuery({ 
-    hl: "en-US", 
-    tz: "480", 
-    req: {
-      comparisonItem: [{ 
+  const trendsCli = new TrendsClient({ cookie });
+  let e = await trendsCli.authQuery({
+    comparisonItem: [
+      { 
           "keyword":"Hi","geo":"","time":"today 12-m"
-      }],
-      category:0,
-      property:""
-    },
+      },
+    ],
   })
   if(e) throw e;
-  e = await trendsCli.getTimeline({ hl: "en-US", tz: 480 })
+  e = await trendsCli.getTimeline()
   if(e) throw e;
   console.log("data:", trendsCli.data.timelineData[0]);
 } catch(e) {
