9dfbb6be1c07c030b307086e89150fd53a14a980
michaelairola <mikeairola@mac.com>
2020-12-25 07:54:12 -0800

    get all categories, not documented yet though

--- templating/makeMaps.js
+++ templating/makeMaps.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const { baseClient } = require('../lib/base_client.js');
 const { geoListUrl, catListUrl, trendsExploreUrl } = require('../lib/constants/urls.js');
 
-// cant reference utils cause then its a loop
+// can't reference utils cause then its a loop
 const getNIDCookie = async () => {
   const res = await axios("https://google.com")
   const cookies = res.headers['set-cookie']
@@ -15,10 +15,35 @@ const getKey = name => {
 	name = name.replaceAll(/\(.*\)/g, " ")
 	name = name.replaceAll(/[ \-’]+/g, " ")
 	name = name.replaceAll(" ", "_")
+	name = name.replaceAll("+", "plus")
 	name = name.replace(/_$/g, "")
 	if(name.match(/^\d/)) name = `_${name}`
 	return name
 }
+
+const writeFile = (obj, list, fileName) => {
+	const listFileName = `./out/${fileName}.txt`
+	fs.writeFile(listFileName, list, () => {
+		console.log(`${listFileName} updated!`)
+	})
+	const mapFileName = `../lib/constants/${fileName}.js`
+	fs.writeFile(mapFileName, obj, () => {
+		console.log(`${mapFileName} updated!`)
+	})	
+}
+
+const flattenChildren = parents => parents.reduce((acc,{ id, name, children }) => {
+	let subchildren = [];
+	if(children) subchildren = flattenChildren(children);
+	acc = [ ...acc, { id, name }, ...subchildren ];
+	return acc;
+},[])
+const sortAndCheckUniqueness = list => list.sort((a,b) => a.id - b.id).reduce((acc, { id, name }) => {
+	const key = getKey(name);
+	if(acc[key] && acc[key] != id) console.log("warning!", acc[key], id)
+	acc[key] = id;
+	return acc;
+}, {});
 
 const getGeos = async (cookie) => {	
 	const { children } = await baseClient(geoListUrl, {}, { headers: { cookie } })
@@ -31,39 +56,27 @@ const getGeos = async (cookie) => {
 		nameList += `\t${key}\n`
 	})
 	objStr += `}\nmodule.exports = { GEOS };`
-	const geoListFileName = `./out/geos.txt`
-	fs.writeFile(geoListFileName, nameList, () => {
-		console.log(`${geoListFileName} updated!`)
-	})
-	const geoMapFileName = `../lib/constants/geos.js`
-	fs.writeFile(geoMapFileName, objStr, () => {
-		console.log(`${geoMapFileName} updated!`)
-	})	
+	writeFile(objStr, nameList, 'geos')
 }
 const getCats = async (cookie) => {	
 	const { children } = await baseClient(catListUrl, {}, { headers: { cookie } })
+	const allCats = sortAndCheckUniqueness(flattenChildren(children));
 	let objStr = 'const CATEGORIES = {\n';
 	objStr += `\tAll: 0,\n`
+	Object.keys(allCats).forEach(key => {
+		objStr += `\t${key}: ${allCats[key]},\n`
+	})
+	objStr += `}\nmodule.exports = { CATEGORIES };`
 	let nameList = "Category Options:\n";
 	children.forEach(({ id, name, children }) => {
 		let key = getKey(name);
-		objStr += `\t${key}: ${id},\n`
 		nameList += `\t${key}\n`
 	})
-	objStr += `}\nmodule.exports = { CATEGORIES };`
-	const geoListFileName = `./out/categories.txt`
-	fs.writeFile(geoListFileName, nameList, () => {
-		console.log(`${geoListFileName} updated!`)
-	})
-	const geoMapFileName = `../lib/constants/categories.js`
-	fs.writeFile(geoMapFileName, objStr, () => {
-		console.log(`${geoMapFileName} updated!`)
-	})	
+	writeFile(objStr, nameList, "categories")
 }
 
 const getTimes = async cookie => {
 	const data = await baseClient(trendsExploreUrl, {}, { headers: { cookie } });
-	// console.log(res)
 	const [ matchStr ] = data.match(/exploreTimePicker = \[.*?\]/)
 	let exploreTimePicker = []
 	eval(matchStr);
@@ -81,19 +94,12 @@ const getTimes = async cookie => {
 	})
 	objStr += `CUSTOM: (startDate, endDate) => \`\${fmtDate(startDate)} \${fmtDate(endDate)}\`,\n`
 	objStr += `}\nmodule.exports = { TIME_RANGES }`
-	const timeListFileName = `./out/time_ranges.txt`
-	fs.writeFile(timeListFileName, list, () => {
-		console.log(`${timeListFileName} updated!`)
-	})	
-	const timeMapFileName = `../lib/constants/time_ranges.js`
-	fs.writeFile(timeMapFileName, objStr, () => {
-		console.log(`${timeMapFileName} updated!`)
-	})	
+	writeFile(objStr,list,"time_ranges")
 }
 const makeMaps = async () => {
 	const cookie = await getNIDCookie();
-	getTimes(cookie);
+	// getTimes(cookie);
 	// getGeos(cookie);
-	// getCats(cookie);
+	getCats(cookie);
 }
 makeMaps();
